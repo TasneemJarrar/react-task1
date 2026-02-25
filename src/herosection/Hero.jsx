ed37b575af901fc52d86fc629bcf0fb83e9dcf13
Tasneem_jarrar <tasneem.a.jarrar@gmail.com>
2026-02-25 23:09:51 +0200

    add navbar

--- src/herosection/Hero.jsx
+++ src/herosection/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero(){
 
   return <>
     <p>Hero</p>
-  </>
+  </>;
 
 }
 
--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,10 +1,50 @@
-import style from './navbar.module.css'
+import style from "./navbar.module.css";
 
-export default function Navbar(){
+export default function Navbar() {
+  console.log(style);
+  return (
+    <>
+      <nav className={`${style.navBgColor} navbar navbar-expand-lg py-4`}>
+        <div className="container">
+          <a className="navbar-brand text-white fw-bold fs-2" href="#">
+            START BOOTSTRAP
+          </a>
+          <button
+            className={`${style.menuBtn} navbar-toggler text-white`}
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            MENU
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="none"
+                stroke="#fff"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                d="M10 5h10M4 12h16M4 19h10"
+              />
+            </svg>
+          </button>
 
-  return <>
-    <h1>Navbar</h1>
-  </>;
-
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="navbar-nav ms-auto">
+              <a class="nav-link active text-white fw-bold" aria-current="page" href="#">PORTFOLIO</a>
+              <a class="nav-link text-white fw-bold ms-3" href="#">ABOUT</a>
+              <a class="nav-link text-white fw-bold ms-3" href="#">CONTACT</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
-
