4d0c5ad56182219a603040bc3c4b6653831de247
Tasneem_jarrar <tasneem.a.jarrar@gmail.com>
2026-02-26 02:23:47 +0200

    add copyrights section

--- src/copyright/Copyright.jsx
+++ src/copyright/Copyright.jsx
@@ -2,6 +2,12 @@ import style from './copyright.module.css'
 
 export default function Copyright() {
   return (
-    <div>Copyright</div>
+    <section className="copyright">
+      <div className={style.copyBg}>
+        <div className="container py-3 d-flex justify-content-center align-items-center">
+          <p className="text-white p-0 m-0">Copyright &copy; Your Website 2023</p>
+        </div>
+      </div>
+    </section>
   )
 }
